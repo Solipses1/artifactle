511a2d1c331ee7d004a68006652742bc75fbb1d5
Jorden <knock007@alumni.umn.edu>
2025-08-05 14:17:12 -0500

    changed build file

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,10 @@
+python3.9 -m venv venv
+
+# activate the virtual environment
+source venv/bin/activate
+
+# install all deps in the venv
 pip install -r requirements.txt
+
+# collect static files using the Python interpreter from venv
 python manage.py collectstatic --noinput
